d6e23318f22c8878b868478e43872c1b2af6bd62
BarryBarky <s1135928@student.hsleiden.nl>
2022-10-19 10:43:55 +0200

    made architecture

---
+++ components/inventory.js
@@ -0,0 +1,44 @@
+import {getButtonWithIcon, getButtonWithText} from "./button.js";
+import {showPopup} from "./popup.js";
+
+const section = document.createElement('section');
+section.classList.add('inventory')
+
+const generateCurrentInventoryItems = () => {
+    const inventory = []
+
+    // You can change the key and the property value to your likings
+
+    if (localStorage.getItem("key1")) {
+        inventory.push(
+            {
+                key: localStorage.getItem("key1"),
+                iconUrl: ``,
+                handler: () => alert(localStorage.getItem("key1"))
+            }
+        )
+    } else {inventory.push({key: null})}
+    if (localStorage.getItem("key2")) {
+        inventory.push(
+            {
+                key: localStorage.getItem("key2"),
+                iconUrl: ``,
+                handler: () => alert(localStorage.getItem("key2"))
+            }
+        )
+    }  else {inventory.push({key: null})}
+
+    console.log(inventory)
+
+    return inventory;
+}
+
+const getInventory = () => {
+    const currentInventoryItems = generateCurrentInventoryItems()
+    currentInventoryItems.map((item) => {
+        item.key ? section.appendChild(getButtonWithIcon(item.iconUrl, item.handler)) : section.appendChild(getButtonWithText('X', () => showPopup('Something went wrong', "You don't have this inventory item available", true)))
+    })
+    return section;
+}
+
+export {getInventory};
---
+++ main.js
@@ -0,0 +1,10 @@
+const wrapper = document.querySelector(".wrapper")
+const frame = document.querySelector(".frame")
+
+
+
+const main = () => {
+
+}
+
+main()
