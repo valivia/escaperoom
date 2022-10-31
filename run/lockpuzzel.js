ee6240e58a90f3be28a53342f86f0e7483fe3e56
RavenMQueen <48282684+RavenMQueen@users.noreply.github.com>
2022-10-31 14:23:50 +0100

    alles pages van mijn puzzeldeel staan nu erin

--- run/lockpuzzel.js
+++ run/lockpuzzel.js
@@ -3,22 +3,21 @@ import {getFrame} from "../../components/frame.js";
 import {getButtonGoBack} from "../../components/button.js";
 
 
+
 //header
-const header = getHeader('You need to go inside the house.', 'The House...');
+const header = getHeader('You need to move the pin to the correct position', 'Time to break in...');
 
 //frame
-const frame = getFrame('assets/frames/intro/house2.svg');
+const frame = getFrame('assets/frames/intro/spel1.svg');
 
 //backbutton
-const backButton = getButtonGoBack('Terug naar start', 'index.html');
+const backButton = getButtonGoBack('back to the house...', 'frontdoor.html');
 
 const assignClickAbles = () =>{
     document.querySelector("object").addEventListener("load", () =>{
         const svg = document.querySelector("object").contentDocument.getElementsByTagName('svg')[0]
         svg.style.width = '100%'
         svg.style.height = '100%'
-            svg.querySelector('#frontdoor').addEventListener("click", () => {window.location.href = 'frontdoor.html'
-        })
     });
 }
 
