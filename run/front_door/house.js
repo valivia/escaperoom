3c794050c5c329553b81e4402dec5d75a4b4e0ee
valivia <valivia@xayania.com>
2022-11-01 20:47:51 +0000

    End pages

--- run/front_door/house.js
+++ run/front_door/house.js
@@ -1,6 +1,6 @@
-import {getHeader, updateStatusHeader} from "../../components/header.js";
-import {getFrame} from "../../components/frame.js";
-import {getButtonGoBack} from "../../components/button.js";
+import { getHeader, updateStatusHeader } from "../../components/header.js";
+import { getFrame } from "../../components/frame.js";
+import { getButtonGoBack } from "../../components/button.js";
 
 
 //header
@@ -10,16 +10,21 @@ const header = getHeader('You need to go inside the house.', 'The House...');
 const frame = getFrame('assets/frames/intro/house2.svg');
 
 //backbutton
-const backButton = getButtonGoBack('Terug naar start', 'index.html');
+const backButton = getButtonGoBack('Back to start menu', 'index.html');
 
-const assignClickAbles = () =>{
-    document.querySelector("object").addEventListener("load", () =>{
+const assignClickAbles = () => {
+    document.querySelector("object").addEventListener("load", () => {
         const svg = document.querySelector("object").contentDocument.getElementsByTagName('svg')[0]
         svg.style.width = '100%'
         svg.style.height = '100%'
         svg.querySelector('#frontdoor').style.cursor = 'pointer';
-            svg.querySelector('#frontdoor').addEventListener("click", () => {window.location.href = 'frontdoor.html'
+        svg.querySelector('#frontdoor').addEventListener("click", () => {
+            window.location.href = 'frontdoor.html'
         })
+
+        if (localStorage.getItem("key1") !== null && localStorage.getItem("key2") !== null) {
+            window.location = "/good_ending.html"
+        }
     });
 }
 
